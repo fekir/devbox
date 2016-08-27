1bfe27cf5ef8facc5af6a2297e30b518f910539b
fekir <federico.kircheis@gmail.com>
2016-08-27 07:46:04 +0200

    added string utility functions for wstring variant (used in windows)

--- lib/string_utils.hpp
+++ lib/string_utils.hpp
@@ -33,18 +33,31 @@
 
 // array with common spaces, used by trim functions. A lot of UTF characters are missing.
 constexpr char char_to_trim[] = { ' ', '\0', '\t', '\n', '\r', '\v', '\f'};
+constexpr wchar_t wchar_to_trim[] = { L' ', L'\0', L'\t', L'\n', L'\r', L'\v', L'\f'};
+
 
 template<class T, size_t N>
 constexpr size_t array_size(const T(&)[N]) noexcept {return N;}
+
+static_assert(array_size(char_to_trim) == array_size(wchar_to_trim), "needs to have same elements, but size is different");
+
 
 inline std::string rtrim(const std::string& s){
 	const auto last = s.find_last_not_of(char_to_trim, std::string::npos, array_size(char_to_trim));
 	return last == std::string::npos ? "" : s.substr(0, last+1);
 }
+inline std::wstring rtrim(const std::wstring& s) {
+	const auto last = s.find_last_not_of(wchar_to_trim, std::wstring::npos, array_size(wchar_to_trim));
+	return last == std::wstring::npos ? L"" : s.substr(0, last + 1);
+}
 
 inline std::string ltrim(const std::string& s){
 	const auto first = s.find_first_not_of(char_to_trim, 0, array_size(char_to_trim));
 	return first == std::string::npos ? "" : s.substr(first);
+}
+inline std::wstring ltrim(const std::wstring& s){
+	const auto first = s.find_first_not_of(wchar_to_trim, 0, array_size(wchar_to_trim));
+	return first == std::wstring::npos ? L"" : s.substr(first);
 }
 
 inline std::string trim(const std::string& s){
@@ -56,6 +69,30 @@ inline std::string trim(const std::string& s){
 	const auto last = s.find_last_not_of(char_to_trim, std::string::npos, array_size(char_to_trim));
 	assert(last >= first); assert(last != std::string::npos);
 	return s.substr(first, last-first+1);
+}
+
+inline std::wstring trim(const std::wstring& s) {
+	// not calling ltrim and rtrim for avoiding one reallocation -> it may be a premature optimisation
+	const auto first = s.find_first_not_of(wchar_to_trim, 0, array_size(wchar_to_trim));
+	if (first == std::wstring::npos) {
+		return L"";
+	}
+	const auto last = s.find_last_not_of(wchar_to_trim, std::wstring::npos, array_size(wchar_to_trim));
+	assert(last >= first); assert(last != std::wstring::npos);
+	return s.substr(first, last - first + 1);
+}
+
+
+
+inline bool start_with(const std::string& big, const std::string& smalls) {
+	return smalls.length() <= big.length()
+		&& std::equal(smalls.begin(), smalls.end(), big.begin());
+}
+
+inline bool end_with(const std::string& big, const std::string& smalls)
+{
+	return smalls.size() <= big.size()
+		&& std::equal(smalls.rbegin(), smalls.rend(), big.rbegin());
 }
 
 /// reads a whole line inside a std::string
@@ -106,9 +143,31 @@ inline std::vector<std::string> split(const std::string& s, const char del = ' '
 	return to_return;
 }
 
+inline std::vector<std::wstring> split(const std::wstring& s, const wchar_t del = L' ') {
+	if (s.empty()) {
+		return{};
+	}
+	std::vector<std::wstring> to_return;
+
+	auto pos_end = s.find(del);
+	auto pos_begin = decltype(pos_end){0};
+
+	while (pos_end != std::wstring::npos) {
+		to_return.emplace_back(s.substr(pos_begin, pos_end - pos_begin));
+		pos_begin = pos_end +1;
+		pos_end = s.find(del, pos_begin);
+	}
+	to_return.emplace_back(s.substr(pos_begin, s.size() - pos_begin));
+	return to_return;
+}
+
 /// use when converting from char* to std::string and char* may be a nullptr, it will be replaced with an empty string
 inline std::string to_string(const char* s){
 	return s == nullptr ? "" : s;
 }
+inline std::wstring to_string(const wchar_t* s) {
+	return s == nullptr ? L"" : s;
+}
+
 
 #endif
